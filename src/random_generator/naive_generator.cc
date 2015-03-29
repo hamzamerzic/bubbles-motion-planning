9d880d686062fe622393d166bc9deba191694606
Hamza Merzić <hamzamerzic@gmail.com>
2015-03-29 17:49:34 +0200

    Added random array generation.

--- src/random_generator/naive_generator.cc
+++ src/random_generator/naive_generator.cc
@@ -1,15 +1,29 @@
 #include "naive_generator.h"
 
 NaiveGenerator::NaiveGenerator(
-    const std::vector<std::pair<double, double> >& limits) {
+    const std::vector<std::pair<double, double> >& limits) :
+    space_dimension_ (limits.size()) {
   for(auto& limit : limits) {
     distributions_.emplace_back(limit.first, limit.second);
   }
+  std::random_device rd;
+  random_engine_.seed(rd());
 }
 
-std::vector<double> NaiveGenerator::NextPoint() {
+std::vector<double> NaiveGenerator::CreatePoint() {
   std::vector<double> point;
   for (auto& distribution : distributions_)
-    point.push_back(distribution(random_device_));
+    point.push_back(distribution(random_engine_));
   return point;
+}
+
+std::unique_ptr<double[]> NaiveGenerator::CreateSampleSpace(size_t num_points) {
+  std::unique_ptr<double[]> sample_space (
+    new double[num_points * space_dimension_]);
+  for (size_t i (0); i < num_points; ++i) {
+    for (size_t k (0); k < distributions_.size(); ++k)
+      sample_space[i * space_dimension_ + k] =
+        distributions_.at(k)(random_engine_);
+  }
+  return sample_space;
 }
--- src/random_generator/naive_generator.h
+++ src/random_generator/naive_generator.h
@@ -4,17 +4,22 @@
 #include <random>
 #include <vector>
 #include <utility>
+#include <memory>
 
-#include "random_point_generator_interface.h"
+#include "random_space_generator_interface.h"
 
-class NaiveGenerator : public RandomPointGeneratorInterface {
+class NaiveGenerator : public RandomSpaceGeneratorInterface {
 public:
   NaiveGenerator(const std::vector<std::pair<double, double> >& limits);
-  std::vector<double> NextPoint();
+  std::vector<double> CreatePoint();
+  // Sample space is kept as single array of points
+  // Array is of [num_points * dimension x 1] dimension
+  std::unique_ptr<double[]> CreateSampleSpace(size_t num_points);
 
 private:
-  std::vector<std::uniform_real_distribution<>> distributions_;
-  std::random_device random_device_;
+  std::vector<std::uniform_real_distribution<double>> distributions_;
+  std::mt19937 random_engine_;
+  size_t space_dimension_;
 };
 
 #endif // NAIVE_GENERATOR_H_INCLUDED
