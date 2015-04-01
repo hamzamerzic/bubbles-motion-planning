85ca5b421e2bfd0680df01c030c455dd8e3b00cc
Hamza Merzić <hamzamerzic@gmail.com>
2015-04-01 12:11:35 +0200

    Added patch to prevent warnings.

--- src/prm_tree.h
+++ src/prm_tree.h
@@ -10,16 +10,21 @@
 class PrmTree {
 public:
   typedef Eigen::VectorXd EVectorXd;
-  PrmTree(PqpEnvironment* pqp_environment):
-    pqp_environment_(pqp_environment),
-    visited_(std::vector<bool>(pqp_environment->sample_space_size(), false)) {}
+  PrmTree(PqpEnvironment* pqp_environment, EVectorXd& start, EVectorXd& end):
+    pqp_environment_(pqp_environment), start_(start), end_(end),
+    space_size_(pqp_environment->sample_space_size() + 2),
+    visited_(std::vector<bool>(space_size_, false)) {}
 
   virtual bool TryConnect(EVectorXd& point1, EVectorXd& point2) = 0;
   virtual bool AddPoint(int point_index) = 0;
+  virtual bool BuildTree() = 0;
 
 protected:
   std::unique_ptr<PqpEnvironment> pqp_environment_;
+  EVectorXd start_, end_;
+  int space_size_;
   std::vector<bool> visited_;
+
 };
 
 #endif // PRM_TREE_H_INCLUDED
